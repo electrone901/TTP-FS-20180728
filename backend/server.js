a98b0e0ff0c5e8b720758ca3739240056131f93b
Mac <Mac@FES-Macbook-081.local>
2018-09-29 19:13:26 -0400

    add user model and registrationroutes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
-
 // Exporting routes
 const userInfo = require('./routes/api/userInfo');
 const login = require('./routes/api/login');
@@ -9,8 +10,21 @@ const signup = require('./routes/api/signup');
 const transactions = require('./routes/api/transactions');
 const companies = require('./routes/api/companies');
 
+// Exporting db
+const db = require('./config/keys').mongoURI;
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+
 
 app.get('/', (req, res) => res.send('Hello, you are on the root directory!'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // Use routes
 app.use('/api', userInfo);
